bfae330b5f1a3be70d1fdab3a65a27249b8a2d2d
javierdosreis <94925907+javierdosreis@users.noreply.github.com>
2021-11-29 17:29:26 -0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import Contador from './contador/contador';
+import Hooks from './contador/Hooks';
+import Hooks2 from './contador/Hooks2';
+import ToDoList from './formulario/formulario'; 
+
+
+const notas = ["amarillo","negro"]
+ReactDOM.render(
+  <React.StrictMode>
+    <Contador />
+    <Hooks />
+    <Hooks2 />
+    <ToDoList array={notas}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
