bd0a066f4803f9ba79e9e1e2939e21501b6ec3a7
javierdosreis <94925907+javierdosreis@users.noreply.github.com>
2021-11-30 17:28:30 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -9,11 +9,32 @@ class Contador extends react.Component{
         numero: this.state.numero +1
         })
     }    
+
+    decrementar = () => {
+        this.setState({
+        numero: this.state.numero -1    
+        })
+        if(this.state.numero <0.5){
+            this.setState({ numero:this.state.numero =0
+        })
+        }
+          
+    
+    
+    }
+    reiniciar =() => {
+        this.setState({
+        numero:this.state.numero =0
+        })
+    }
     render(){
         return(
             <div>
               <h1>{this.state.numero}</h1>
-              <button onClick={this.incrementar}>+</button>
+              <button className="botones" onClick={this.incrementar}> + </button>
+              <button className="botones" onClick={this.decrementar}> - </button>
+              <button className="botones" onClick={this.reiniciar}>reiniciar</button>
+        
             </div>
         )
 }
