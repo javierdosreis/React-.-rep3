638adcf5baebb8c95f06114ef7c930d81b746098
javierdosreis <94925907+javierdosreis@users.noreply.github.com>
2021-12-03 17:26:53 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-useEffect(() => { 
-  const url = "https://rickandmortyapi.com/api/character" 
-  
-},[]);
+function App() {
+  return (
+    <>
+      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+}
 
-return <div></div>;
-};
-
-export default App
+export default App;
